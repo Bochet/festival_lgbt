59b67e93f0ea76ad31abff00d80650101cf0c5cc
Guillaume Bochet <bochet@aom19.etu>
2013-01-30 13:56:46 +0100

    Projet wp

---
+++ wp-content/themes/hero/page.php
@@ -0,0 +1,50 @@
+<?php get_header(); ?>
+		
+		<div id="subhead_container">
+
+		<div id="subhead_wrapper">		
+			<div id="subhead">
+		
+		<h1><?php the_title(); ?></h1>
+			
+			</div>
+			
+			
+				<div class="clear"></div>
+			
+		</div>
+			<hr />
+	</div>		
+	
+		<!--content-->
+		<div id="content_container">
+			
+			<div id="content">
+		
+				<div id="left-col">
+		
+
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+					
+					<div class="post-entry">
+
+						<?php the_content(); ?>
+						<div class="clear"></div>
+						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'Hero' ), 'after' => '' ) ); ?>
+						
+					</div><!--post-entry end-->
+					
+					<?php comments_template( '', true ); ?>
+
+<?php endwhile; ?>
+</div> <!--left-col end-->
+
+<?php get_sidebar(); ?>
+
+	</div> 
+</div>
+<!--content end-->
+	
+</div>
+<!--wrapper end-->
+<?php get_footer(); ?>
